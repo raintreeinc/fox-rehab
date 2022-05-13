ead6f94d72a4e3b49ea5a2b39258e2b85e03b1a2
Noah Krohn <102704593+noah-raintree@users.noreply.github.com>
2022-05-13 13:17:00 -0400

    fix keypair format

--- keypair.tf
+++ keypair.tf
@@ -5,5 +5,5 @@ resource "tls_private_key" "this" {
 
 resource "aws_key_pair" "this" {
   key_name = "KP-${upper(local.local_data.tag_prefix)}-${upper(local.local_data.tag_env)}-${upper(local.local_data.tag_client)}"
-  public_key = tls_private_key.this.public_key_pem
+  public_key = tls_private_key.this.public_key_openssh
 }
